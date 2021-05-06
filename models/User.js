6d6cd30fc0ee9c2a6d9d6c50bb08d96dd2663fe1
medium0fry <medium0fry@gmail.com>
2021-05-06 14:28:24 -0400

    Updated User model to work with local auth and added hashing capability

--- models/User.js
+++ models/User.js
@@ -1,14 +1,55 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
-   microsoftId: {
-      type: String,
-      require: true,
-   },
-   displayName: {
+   email: {
       type: String,
       required: true,
-   }
+      unique: true,
+   },
+   userName: {
+      type: String,
+      required: true,
+      unique: true,
+   },
+   password: {
+      type: String,
+      required: true,
+   },
+   created: {
+      type: Date,
+      required: true,
+      default: Date.now,
+   },
+   admin: {
+      type: Boolean,
+      required: true,
+      default: false,
+   },
 });
+
+// Password hash middleware.
+UserSchema.pre('save', function save(next) {
+   const user = this;
+   if (!user.isModified('password')) return next();
+   bcrypt.genSalt(10, (err, salt) => {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, (err, hash) => {
+         if (err) return next(err);
+         user.password = hash;
+         next();
+      });
+   });
+});
+
+// Helper to validate user's password
+UserSchema.methods.comparePassword = function comparePassword(
+   candidatePassword,
+   cb
+) {
+   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+      cb(err, isMatch);
+   });
+};
 
 module.exports = mongoose.model('User', UserSchema);
