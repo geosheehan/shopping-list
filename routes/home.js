0ec18c563c046463930cab3b06481f24ea15f556
medium0fry <medium0fry@gmail.com>
2021-05-07 21:17:59 -0400

    Added register and logout routes

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const homeController = require('../controllers/home');
 const authController = require('../controllers/auth');
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
 const router = express.Router();
 
 // @desc    Show the landing page
 // @route   GET /
-router.get('/', homeController.getIndex);
+router.get('/', ensureGuest, homeController.getIndex);
 
 // @desc    Show the login page
 // @route   GET /login
@@ -15,5 +16,17 @@ router.get('/login', authController.getLogin);
 // @desc    Process the login page
 // @route   POST /login
 router.post('/login', authController.postLogin);
+
+// @desc    Process logout
+// @route   GET /logout
+router.get('/logout', authController.logout);
+
+// @desc    Show the sign up page
+// @route   GET /register
+router.get('/register', authController.getRegister);
+
+// @desc    Process the sign up page
+// @route   POST /register
+router.post('/register', authController.postRegister);
 
 module.exports = router;
