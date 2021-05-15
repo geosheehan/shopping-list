caaf3879536e5c49473a29e1ce32239beee31590
medium0fry <medium0fry@gmail.com>
2021-05-15 18:58:25 -0400

    Added initial components

---
+++ client/src/components/List/List.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Item from './Item/Item.js';
+
+function List() {
+   return (
+      <div>
+         <Item />
+         <button>Add</button>
+      </div>
+   );
+}
+
+export default List
